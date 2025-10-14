7bf3884e772e943bc7f4628d1ba4d31d97bfcfe0
srikanth-sporada <srikanth.n@sporadasecure.com>
2025-10-14 18:41:20 +0530

    env.prod.ts file added

---
+++ dhrm_sporada_V1.0/src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+export const environment = {
+  production: true,
+  path: "https://rane.ssipl.org",
+  path2: "https://rane.ssipl.org",
+
+  // path: 'http://10.102.2.11:5000',
+  // path2:'http://10.102.2.11:5000',
+
+  // path: 'http://10.51.12.166:5000',
+  // path2:'http://10.51.12.166:5000',
+  prodLink: "dhrm", 
+  // path2:'http://14.99.10.243:3000'
+
+  // Canteen_Report: 'https://app.powerbi.com/view?r=eyJrIjoiMzYwNWUxOGItZGRlNy00NzM3LWJiOGUtZjFlYzIxMTM2N2FkIiwidCI6IjQwYTBmOGU1LWZkMDUtNDNmMC05MzkwLThkOTRkZmVmOTgyNSJ9',
+  // HR_link: 'https://app.powerbi.com/view?r=eyJrIjoiNzE4MGIwYTMtZWVlZC00NzY3LWExM2MtZTM4MDIxZDlmZjVmIiwidCI6IjQwYTBmOGU1LWZkMDUtNDNmMC05MzkwLThkOTRkZmVmOTgyNSJ9',
+  Corp_HR_Link : 'https://app.powerbi.com/view?r=eyJrIjoiYjVmMTRjNWItYTQzMi00MjY4LWEyZDYtYzZjOWJiZTgwNWNjIiwidCI6IjQwYTBmOGU1LWZkMDUtNDNmMC05MzkwLThkOTRkZmVmOTgyNSJ9'
+
+};
+
+
+
+export const powerBiLink = {
+  Canteen_Report: 'https://app.powerbi.com/view?r=eyJrIjoiZTcyYzNmYjQtZTg2OS00ZWQzLWJlYWUtOGJmMDQ3ZWM0YjAwIiwidCI6IjQwYTBmOGU1LWZkMDUtNDNmMC05MzkwLThkOTRkZmVmOTgyNSJ9',
+  HR_link: 'https://app.powerbi.com/view?r=eyJrIjoiNzE4MGIwYTMtZWVlZC00NzY3LWExM2MtZTM4MDIxZDlmZjVmIiwidCI6IjQwYTBmOGU1LWZkMDUtNDNmMC05MzkwLThkOTRkZmVmOTgyNSJ9',
+  Corp_HR_Link : 'https://app.powerbi.com/view?r=eyJrIjoiYjVmMTRjNWItYTQzMi00MjY4LWEyZDYtYzZjOWJiZTgwNWNjIiwidCI6IjQwYTBmOGU1LWZkMDUtNDNmMC05MzkwLThkOTRkZmVmOTgyNSJ9'
+}
